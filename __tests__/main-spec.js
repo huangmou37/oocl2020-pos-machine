660d267b8d6c90b82ab116729ecde90b249b2e2e
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-07-09 16:05:52 +0800

    Implement createReceipt

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,7 @@
 const main = require('../main');
 
+// test case of countBarcode
+
 it ('should return count of barcode correctly when count barcode given [0001, 0002, 0001]', () => {
   let barcodes = ['0001', '0002', '0001'];
   let outputBarcodeCount = main.countBarcode(barcodes);
@@ -7,5 +9,28 @@ it ('should return count of barcode correctly when count barcode given [0001, 00
   expect(outputBarcodeCount).toEqual({
     '0001': 2,
     '0002': 1
+  });
+});
+
+//  test case of createReceipt
+
+it ('should return receipt object correctly when create receipt given [0001, 0002, 0001]', () => {
+  let barcodes = ['0001', '0002', '0001'];
+  let outputReceipt = main.createReceipt(barcodes);
+
+  expect(outputReceipt).toEqual({
+    receiptItems: [
+      {
+        name: 'Coca Cola',
+        totalPrice: 6,
+        quantity: 2
+      },
+      {
+        name: 'Diet Coke',
+        totalPrice: 4,
+        quantity: 1
+      }
+    ],
+    price: 10
   });
 });
--- main.js
+++ main.js
@@ -10,6 +10,59 @@ function countBarcode(barcodes) {
   }, {});
 }
 
+function createReceipt(barcodes) {
+  let productDict = {};
+  loadAllProduct().forEach(product => {
+    productDict[product.id] = product;
+  });
+
+  // if (barcodes.some(barcode => !(barcode in productDict))) {
+  //   throw 'invalid barcode'
+  // }
+
+  let barcodeCountDict = countBarcode(barcodes);
+
+  let receipt = {receiptItems: [], price: 0};
+
+  for (let barcode in barcodeCountDict) {
+    if (productDict.hasOwnProperty(barcode)) {
+      let productName = productDict[barcode].name;
+      let productQuantity = barcodeCountDict[barcode];
+      let productTotalPrice = productDict[barcode].price * productQuantity;
+      let receiptItem = {
+        name: productName,
+        totalPrice: productTotalPrice,
+        quantity: productQuantity
+      };
+      receipt.receiptItems.push(receiptItem)
+    }
+  }
+
+  receipt.receiptItems.forEach(item => receipt.price += item.totalPrice);
+
+  receipt.price = receipt.receiptItems.reduce((tmpPrice, item) => {
+    return tmpPrice + item.totalPrice;
+  }, 0);
+
+  return receipt;
+}
+
+function loadAllProduct() {
+  return [
+    {"id": "0001", "name" : "Coca Cola", "price": 3},
+    {"id": "0002", "name" : "Diet Coke", "price": 4},
+    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name" : "Mountain Dew", "price": 6},
+    {"id": "0005", "name" : "Dr Pepper", "price": 7},
+    {"id": "0006", "name" : "Sprite", "price": 8},
+    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name" : "Fanta", "price": 12}
+  ];
+}
+
 module.exports = {
+  createReceipt,
   countBarcode
 };
